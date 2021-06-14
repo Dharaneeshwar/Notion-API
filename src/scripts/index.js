918e9b9885a2f97edc3c1cb7d5ea9801bd402d72
Dharaneeshwar <daranip@gmail.com>
2021-06-14 18:10:54 +0530

    Contact Form Design

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,4 +4,9 @@ if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
 
+// var generatePassword = require('password-generator');
+// console.log("here is the passwor",generatePassword()," ");
+
+// const { Client } = require('@notionhq/client');
+
 console.log('webpack starterkit');
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -42,6 +42,7 @@ module.exports = {
           loader: 'file-loader',
           options: {
             name: '[path][name].[ext]',
+            publicPath:'./'
           },
         },
       },
